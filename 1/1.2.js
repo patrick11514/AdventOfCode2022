ca914ccb8b6e947077e8387a05cd3ddae678ae35
patrick11514 <ja@patrick115.eu>
2022-12-01 11:32:34 +0100

    Day 1

---
+++ 1/1.2.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+let elfs = []
+
+let data = fs.readFileSync('input.txt', 'utf8')
+
+let lines = data.split('\n')
+
+console.log(lines)
+
+let currentCalories = 0
+
+for (let line of lines) {
+    if (line != '') {
+        currentCalories += parseInt(line)
+    } else {
+        elfs.push(currentCalories)
+        currentCalories = 0
+    }
+}
+//last elf
+elfs.push(currentCalories)
+
+let totalCalories = 0
+for (let i = 0; i < 3; i++) {
+    let mostCalories = 0
+    let mostId = 0
+    for (let index in elfs) {
+        let elf = elfs[index]
+        if (elf > mostCalories) {
+            mostCalories = elf
+            mostId = index
+        }
+    }
+    totalCalories += mostCalories
+    elfs[mostId] = 0
+}
+
+console.log(totalCalories)
